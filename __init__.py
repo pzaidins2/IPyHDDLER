554b91aa95ba26ee2dea9b8949f8d03bca73f9c4
pzaidins2 <paulzaidins2@gmail.com>
2023-12-01 16:57:08 -0500

    packaging

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+Project:
+    IPyHDDLER - Parser for HDDL -> IPyHOPPER
+    Author: Paul Zaidins
+    Copyright (c) 2023, Paul Zaidins
+
+"""
+
+from hddl_parser import HDDL_Parser
+
+"""
+Author(s): Paul Zaidins
+Repository: https://github.com/pzaidins2/IPyHOPPER.git
+"""
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='IPyHDDLER',
+    version='',
+    packages=[ 'ipyhop' ],
+    url='https://github.com/pzaidins2/IPyHOPPER.git',
+    license='',
+    author='paulz',
+    author_email='pzaidins@umd.edu',
+    description='IPyHOPPER HTN Planner',
+    install_requires=[
+        "numpy",
+        "networkx",
+        "matplotlib"
+    ]
+)
